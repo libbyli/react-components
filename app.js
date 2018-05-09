5d99c1a7d788395fb68e98020e27de6551cd1181
Elizabeth Li <sincerelylibby@gmail.com>
2018-05-09 12:13:53 -0700

    Created dynamic grocery list.

--- app.js
+++ app.js
@@ -1,1 +1,31 @@
-// TODO
+var GroceryList = () => {
+  return (
+    <div>
+      {/* <Eggs />
+      <Spinach /> */}
+      <GroceryListItem list = {list} />
+    </div>
+  )
+};
+
+// var Eggs = () => (
+//   <li>eggs</li>
+// );
+
+// var Spinach = () => (
+//   <li>spinach</li>
+// );
+
+const list = ['eggs', 'spinach'];
+
+var GroceryListItem = (props) => {
+  const list = props.list;
+  const listItems = list.map((item) =>
+    <li key = {item}>{item}</li>
+  );
+  return (
+    <ul>{listItems}</ul>
+  );
+}
+
+ReactDOM.render(<GroceryList />, document.getElementById("app"));
