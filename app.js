92de26b9f45dcffcf4f9dce9e0bf5af2edbc5d73
libbyli <sincerelylibby@gmail.com>
2018-05-10 11:49:37 -0700

    Implemented state and hover/click functionality.

--- app.js
+++ app.js
@@ -14,11 +14,40 @@ var GroceryList = (props) => (
 class GroceryListItem extends React.Component {
   constructor(props) {
     super(props);
+    
+    this.state = {
+      done: false,
+      hover: false
+    };
+  }
+  
+  onListItemClick() {
+    this.setState({
+      done: !this.state.done
+    });
+  }
+  
+  onListItemOn() {
+    this.setState({
+      hover: true
+    })
+  }
+  
+  onListItemOff() {
+    this.setState({
+      hover: false
+    })
   }
 
   render() {
+    
+    var style = {
+      textDecoration: this.state.done ? 'line-through' : 'none',
+      fontWeight: this.state.hover ? 'bold' : 'normal',
+    };
+    
     return (
-      <li>{this.props.item}</li>
+      <li style={style} onClick={this.onListItemClick.bind(this)} onMouseEnter={this.onListItemOn.bind(this)} onMouseLeave={this.onListItemOff.bind(this)}>{this.props.item}</li>
     );
   }
 }
